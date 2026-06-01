923e41b2dd1e88f9d8c50727f24477e00ea31a3d
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2026-06-01 13:16:25 +0530

    urls changed

--- payment/urls.py
+++ payment/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CreateOrderView, VerifyPaymentView, RazorpayWebhookView
 
 urlpatterns = [
-    path("billing/order/create/", CreateOrderView.as_view()),
-    path("billing/payment/verify/", VerifyPaymentView.as_view()),
-    path("billing/webhook/razorpay/", RazorpayWebhookView.as_view()),
+    path("/order/create/", CreateOrderView.as_view()),
+    path("/payment/verify/", VerifyPaymentView.as_view()),
+    path("/webhook/razorpay/", RazorpayWebhookView.as_view()),
 ]
